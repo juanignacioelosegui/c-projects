6ce2d58868092412002068234f04ad2468ae1c9c
Juani Elosegui <juanoelosegui@gmail.com>
2026-02-05 19:25:31 -0300

    Avance en manejo de ventana

--- sistema-votacion/sistema-votacion.c
+++ sistema-votacion/sistema-votacion.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 
 #define WIDTH 900
@@ -6,15 +7,27 @@
 
 int main ( int argc, const char * argv[] )
 {
-    int SDL_Init();
-    
-    SDL_Window * window = 
-        SDL_CreateWindow(
-                "Sistema de Votación",
-                SDL_WINDOWPOS_CENTERED,
-                SDL_WINDOWPOS_CENTERED,
-                WIDTH, HEIGHT, 0);
+    if ( SDL_Init(SDL_INIT_EVERYTHING) < 0 ) { printf( "[DBG] SDL couldn't initialise! Error: %s\n", SDL_GetError() ); }
 
-    printf("Hello world!\n");
-    return 0;
+    SDL_Window * window = SDL_CreateWindow( 
+            "Sistema de Votación",
+            SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            WIDTH, HEIGHT, 0);
+    
+    if ( window == NULL ) { printf( "[DBG] Window couldn't be created! Error: %s\n", SDL_GetError() ); }
+
+    SDL_Event e;
+    while ( true )
+    {
+        if ( SDL_PollEvent(&e) )
+        {
+            if ( SDL_QUIT == e.type ) break;
+        }
+    }
+
+    SDL_DestroyWindow( window );
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
